8f4db042a002c254add878383f10cd38589d2982
Mark Miyashita <negativetwelve@gmail.com>
2017-08-22 16:42:29 -0700

    Remove presets from the babel-preset-jolt which are no longer used

--- packages/babel-preset-jolt/src/preset.js
+++ packages/babel-preset-jolt/src/preset.js
@@ -1,8 +1,3 @@
-// Presets
-import presetES2015 from 'babel-preset-es2015';
-import presetES2016 from 'babel-preset-es2016';
-import presetES2017 from 'babel-preset-es2017';
-
 // Plugins
 import pluginTransformBuiltinExtend from 'babel-plugin-transform-builtin-extend';
 import pluginTransformClassProperties from 'babel-plugin-transform-class-properties';
@@ -14,11 +9,6 @@ import pluginTransformObjectRestSpread from 'babel-plugin-transform-object-rest-
 // eslint-disable-next-line
 export default (context, options = {}) => {
   return {
-    presets: [
-      presetES2015,
-      presetES2016,
-      presetES2017,
-    ],
     plugins: [
       [pluginTransformBuiltinExtend, {
         globals: ['Array', 'Error'],
