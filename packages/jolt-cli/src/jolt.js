2e2b444115874fb291324520c95a1a6f9d45eb93
Mark Miyashita <negativetwelve@gmail.com>
2017-08-23 15:45:34 -0700

    Remove version check, create self-update command for jolt

---
+++ packages/jolt-cli/src/jolt.js
@@ -0,0 +1,24 @@
+// Libraries
+import commander from 'commander';
+
+// CLI
+import Package from '../package.json';
+import {COMMAND} from './config';
+import {log} from './utils';
+
+
+/**
+ * Defines the main program. Adds several options to the CLI:
+ *
+ *   -v, --version: Prints out the package version.
+ *   -h, --help: Prints out the extended help message + usage.
+ *
+ */
+new commander.Command(COMMAND)
+  .version(Package.version, '-v, --version')
+  .arguments('<command>')
+  .usage('<command> [options]')
+  .on('--help', () => log(''))
+  .command('help', 'displays commands and options')
+  .command('self-update', 'updates the CLI and all dependencies')
+  .parse(process.argv);
