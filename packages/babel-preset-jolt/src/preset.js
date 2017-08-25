7b4c086ef624dd2c4e0cfbc44fc749dce3ef07cd
Mark Miyashita <negativetwelve@gmail.com>
2017-08-25 02:33:47 -0700

    Add target option to the babel-preset-jolt

--- packages/babel-preset-jolt/src/preset.js
+++ packages/babel-preset-jolt/src/preset.js
@@ -1,24 +1,40 @@
+// Presets
+import presetEnv from 'babel-preset-env';
+import presetReactNative from 'babel-preset-react-native';
+
 // Plugins
-import pluginTransformBuiltinExtend from 'babel-plugin-transform-builtin-extend';
-import pluginTransformClassProperties from 'babel-plugin-transform-class-properties';
-import pluginTransformDecoratorsLegacy from 'babel-plugin-transform-decorators-legacy';
-import pluginTransformExportExtensions from 'babel-plugin-transform-export-extensions';
-import pluginTransformObjectRestSpread from 'babel-plugin-transform-object-rest-spread';
+import transformBuiltinExtend from 'babel-plugin-transform-builtin-extend';
+import transformClassProperties from 'babel-plugin-transform-class-properties';
+import transformDecoratorsLegacy from 'babel-plugin-transform-decorators-legacy';
+import transformExportExtensions from 'babel-plugin-transform-export-extensions';
+import transformObjectRestSpread from 'babel-plugin-transform-object-rest-spread';
+import transformRuntime from 'babel-plugin-transform-runtime';
 
 
-// eslint-disable-next-line
-export default (context, options = {}) => {
+export default (context, options = {}) => { // eslint-disable-line
+  const {target} = options;
+
+  // These are the plugins that are shared with all targets.
+  const plugins = [
+    [transformBuiltinExtend, {
+      globals: ['Array', 'Error'],
+    }],
+
+    // NOTE(mark): Decorators MUST come before class properties.
+    transformDecoratorsLegacy,
+    transformClassProperties,
+    transformExportExtensions,
+    transformObjectRestSpread,
+  ];
+
   return {
+    presets: [
+      target === 'node' && presetEnv({targets: {node: 4}}),
+      target === 'react-native' && presetReactNative,
+    ].filter(Boolean),
     plugins: [
-      [pluginTransformBuiltinExtend, {
-        globals: ['Array', 'Error'],
-      }],
-
-      // NOTE(mark): Decorators MUST come before class properties.
-      pluginTransformDecoratorsLegacy,
-      pluginTransformClassProperties,
-      pluginTransformExportExtensions,
-      pluginTransformObjectRestSpread,
-    ],
+      target === 'node' && transformRuntime,
+      ...plugins,
+    ].filter(Boolean),
   };
 }
