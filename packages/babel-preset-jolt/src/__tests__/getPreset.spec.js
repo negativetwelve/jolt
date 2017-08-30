84fdaef65e0ea74f50609f47935dd60f1ab88276
Mark Miyashita <negativetwelve@gmail.com>
2017-08-29 19:01:55 -0700

    Add support for the uglify option for the 'env'. Add tests to make sure it's being set properly

--- packages/babel-preset-jolt/src/__tests__/getPreset.spec.js
+++ packages/babel-preset-jolt/src/__tests__/getPreset.spec.js
@@ -6,7 +6,8 @@ import getPreset from '../getPreset';
 describe('babel-preset-jolt', () => {
   set('target', () => undefined);
   set('useStaticImports', () => false);
-  set('options', () => ({target, import: {static: useStaticImports}}));
+  set('uglify', () => false);
+  set('options', () => ({target, uglify, import: {static: useStaticImports}}));
   set('preset', () => getPreset(null, options));
 
   context('with no target', () => {
@@ -70,6 +71,14 @@ describe('babel-preset-jolt', () => {
 
     it('should contain plugins', () => {
       expect(preset.plugins).not.toHaveLength(0);
+    });
+
+    context('with uglify option set to true', () => {
+      set('uglify', () => true);
+
+      it('should include uglify in the targets', () => {
+        expect(preset.presets[0][1].targets.uglify).toEqual(uglify);
+      });
     });
   });
 
